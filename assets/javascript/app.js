90661aa4a1eac99db2727d971d7202b74085a62e
ava krishn <avakrishn@gmail.com>
2018-06-27 15:58:18 -0700

    added array of objects that holds the questions and answers for the trivia game

---
+++ assets/javascript/app.js
@@ -0,0 +1,256 @@
+var movies = [
+    {
+        id: 0,
+        question: "&#x1F427;  &#x1F603; &#x1F463;",
+        answers:{
+            a: "FINDING YOUR FEET",
+            b: "MARCH OF THE PENGUINS",
+            c: "HAPPY FEET",
+            d: "PENGUINS OF MADAGASCAR",
+        },
+        correct: "HAPPY FEET",
+    },
+
+    {   id: 1,
+        question: "&#x1F5E1; &#x1F355; &#x1F42D; &#x1F422",
+        answers:{
+            a: "THE SECRET LIFE OF PETS",
+            b: "TEENAGE MUTANT NINJA TURTLES",
+            c: "A TURTLES TALE",
+            d: "TURTLE, THE INCREDIBLE JOURNEY",
+        },
+        correct: "TEENAGE MUTANT NINJA TURTLES",
+    },
+
+
+
+    {   id: 2,
+        question: "&#x1F46A; &#x1F6EB; &#x1F631; &#x1F3E1; &#x1F384;",
+        answers:{
+            a: "HOME ALONE",
+            b: "FLY AWAY HOME",
+            c: "FAMILY FLIGHT",
+            d: "THE NIGHTMARE BEFORE CHRISTMAS",
+        },
+        correct: "HOME ALONE",
+           
+    },
+
+    {   
+        id: 3,
+        question: "&#x1F62E; &#x1F417; &#x1F50E; &#x1F4BC; &#x1F4DA;",
+        answers:{
+            a: "JUMANJI",
+            b: "FANTASTIC BEASTS AND WHERE TO FIND THEM",
+            c: "POKÈMON",
+            d: "SCOOBY-DOO",
+        },
+        correct: "FANTASTIC BEASTS AND WHERE TO FIND THEM",
+           
+    },
+
+    {   
+        id: 4,
+        question: "&#x1F699; &#x1F4A8; &#x23F3; ",
+        answers:{
+            a: "FAST AND FURIOUS",
+            b: "CARS",
+            c: "FAST FIVE",
+            d: "RUSH HOUR",
+        },
+        correct: "RUSH HOUR",
+           
+    },
+
+
+    {
+        id: 5,
+        question: "&#x1F3F0; &#x1F3D4; &#x1F46D; &#x2603&#xFE0F;",
+        answers:{
+            a: "ICE PRINCESS",
+            b: "JACK FROST",
+            c: "FROZEN",
+            d: "THE PRINCESS DIARIES",
+        },
+        correct: "FROZEN",
+           
+    },
+
+    {
+        id: 6,
+        question: "&#x1F469&#x1F3FF; &#x1F469&#x1F3FF; &#x1F469&#x1F3FF; &#x1F680;",
+        answers:{
+            a: "HIDDEN FIGURES",
+            b: "DREAM GIRLS",
+            c: "MISSION TO MARS",
+            d: "SPARKLE",
+        },
+        correct: "HIDDEN FIGURES",
+           
+    },
+
+    {
+        id: 7,
+        question: "&#x1F467; &#x1F49A; &#x1F608; &#x1F497; &#x1F384;",
+        answers:{
+            a: "THE NIGHTMARE BEFORE CHRISTMAS",
+            b: "THE GRINCH",
+            c: "ELF",
+            d: "GREMLINS",
+        },
+        correct: "THE GRINCH",
+           
+    },
+
+    {
+        id: 8,
+        question: "&#x1F466&#x1F3FC; &#x1F570; &#x1F511; &#x1F4D4 &#x1F682;",
+        answers:{
+            a: "HUGO",
+            b: "THE POLAR EXPRESS",
+            c: "SOURCE CODE",
+            d: "MURDER ON THE ORIENT EXPRESS",
+        },
+        correct: "HUGO",
+           
+    },
+
+    {
+        id: 9,
+        question: "&#x1F601; &#x1F49B; &#x1F62D; &#x1F499; &#x1F620; &#x2764&#xFE0F; &#x1F61F; &#x1F49C; &#x1F637; &#x1F49A;",
+        answers:{
+            a: "SNOW WHITE AND THE SEVEN DWARFS",
+            b: "POWER RANGERS",
+            c: "INSIDE OUT",
+            d: "CHARLIE AND THE CHOCOLATE FACTORY",
+        },
+        correct: "INSIDE OUT",
+           
+    },
+
+    {
+        id: 10,
+        question: "&#x1F47D; &#x260E&#xFE0F; &#x1F3E0; &#x1F6B2; &#x1F319;",
+        answers:{
+            a: "MEN IN BLACK",
+            b: "10 CLOVERFIELD LANE",
+            c: "ARRIVAL",
+            d: "ET",
+        },
+        correct: "ET",
+           
+    },
+
+    {
+        id: 11,
+        question: "&#x1F469&#x1F3FB; &#x1F306; &#x1F373;  &#x1F48D",
+        answers:{
+            a: "ROMAN HOLIDAY",
+            b: "MY FAIR LADY",
+            c: "THE ENGAGEMENT RING",
+            d: "BREAKFAST AT TIFFANY'S",
+        },
+        correct: "BREAKFAST AT TIFFANY'S",
+           
+    },
+
+    {
+        id: 12,
+        question: "&#x1F437; &#x1F33A; &#x1F334; &#x1F30A;",
+        answers:{
+            a: "LILO & STITCH",
+            b: "BABE",
+            c: "MOANA",
+            d: "CHARLOTTE'S WEB",
+        },
+        correct: "MOANA",
+    },
+
+    {
+        id: 13,
+        question: "&#x1F981; &#x1F682; &#x1F1EE&#x1F1F3;  &#x27A1&#xFE0F; &#x1F1E6&#x1F1FA; &#x1F62D;",
+        answers:{
+            a: "LION KING",
+            b: "SLUMDOG MILLIONAIRE",
+            c: "LIFE OF PI",
+            d: "LION",
+        },
+        correct: "LION",
+    },
+
+    {
+        id: 14,
+        question: "&#x1F6AB; &#x1F57A; &#x1F483; &#x1F6AB; &#x1F3A4; &#x1F3B6;",
+        answers:{
+            a: "LA LA LAND",
+            b: "HAIRSPRAY",
+            c: "GREASE",
+            d: "FOOTLOOSE",
+        },
+        correct: "FOOTLOOSE",
+    },
+
+    {
+        id: 15,
+        question: "&#x1F430; &#x1F694; &#x1F98A; &#x1F454;",
+        answers:{
+            a: "SECRET LIFE OF PETS",
+            b: "DR. DOLITTLE",
+            c: "ZOOTOPIA",
+            d: "PETER RABBIT",
+        },
+        correct: "ZOOTOPIA",
+    },
+
+    {
+        id: 16,
+        question: "&#x2B1B; &#x1F577; &#x1F916; &#x1F468; &#x1F621; &#x1F34F; &#x1F6E1&#xFE0F; &#x1F1FA&#x1F1F8; &#x1F528; &#x26A1;",
+        answers:{
+            a: "JUSTICE LEAGUE",
+            b: "THE AVENGERS",
+            c: "TRANSFORMERS",
+            d: "POWER RANGERS",
+        },
+        correct: "THE AVENGERS",
+    },
+
+    {
+        id: 17,
+        question: "&#x1F470; &#x1F935; &#x1F3A4; &#x1F3B6;",
+        answers:{
+            a: "WEDDING CRASHERS",
+            b: "MAMA MIA",
+            c: "THE WEDDING SINGER",
+            d: "BRIDESMAIDS",
+        },
+        correct: "THE WEDDING SINGER",
+    },
+
+    {
+        id: 18,
+        question: "&#x1F47B; &#x1F47B; &#x1F6AB; &#x1F392; &#x1F52B",
+        answers:{
+            a: "BEETLEJUICE",
+            b: "GOOSEBUMPS",
+            c: "CASPER",
+            d: "GHOSTBUSTERS",
+        },
+        correct: "GHOSTBUSTERS",
+    },
+
+    {
+        id: 19,
+        question: "&#x1F436; &#x1F6B2; &#x1F32A&#xFE0F; &#x1F3DA&#xFE0F; &#x1F460; &#x1F916; &#x1F981; &#x2747&#xFE0F;",
+        answers:{
+            a: "MALEFICENT",
+            b: "OZ THE GREAT AND POWERFUL",
+            c: "THE WIZARD OF OZ",
+            d: "ALICE IN WONDERLAND",
+        },
+        correct: "THE WIZARD OF OZ",
+    },
+   
+    
+]
+
+var questionTitle = "Can you guess the movie from the emojis?"
